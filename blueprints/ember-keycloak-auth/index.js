26a3d5d543601046b715702835a8c890dfcf172e
Stephen Flynn <stephen.flynn@jftechnology.com>
2016-07-03 02:21:02 +0100

    added normalizeEntityName to blueprint

--- blueprints/ember-keycloak-auth/index.js
+++ blueprints/ember-keycloak-auth/index.js
@@ -4,12 +4,17 @@ module.exports = {
 
   description: 'ember-keycloak-auth',
 
+  normalizeEntityName: function () {
+    // this prevents an error when the entityName is
+    // not specified (since that doesn't actually matter
+    // to us
+  },
+
   afterInstall: function () {
 
     return this.addBowerPackagesToProject([
-      { name: 'keycloak' }
+      {name: 'keycloak'}
     ]);
-
   }
 
 };
