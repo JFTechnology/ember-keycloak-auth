9f92982fa6695fd891802a852c336320b08a0ff2
Stephen Flynn <stephen.flynn@jftechnology.com>
2018-06-01 08:52:14 +0100

    Update Ember CLI 3.1

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -4,8 +4,8 @@ import { merge } from '@ember/polyfills';
 import { run } from '@ember/runloop';
 
 export default function startApp(attrs) {
+
   let attributes = merge({}, config.APP);
-  attributes.autoboot = true;
   attributes = merge(attributes, attrs); // use defaults, but you can override;
 
   return run(() => {
