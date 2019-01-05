fdef01de4390095a8a02eee038c7eec4c8cd9fcd
Stephen Flynn <stephen.flynn@jftechnology.com>
2019-01-05 19:30:49 +0000

    fix: dummy app uses ember-cookies - removed ember-cli-js-cookie

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -3,32 +3,55 @@
  */
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
-import Cookies from 'ember-cli-js-cookie';
+import { computed } from '@ember/object';
+const { keys } = Object;
 
 export default Controller.extend({
 
-  session: service('keycloak-session'),
+  keycloakSession: service(),
 
-  url: Cookies.get('keycloak-url'),
+  cookies: service(),
 
-  realm: Cookies.get('keycloak-realm'),
+  allCookies: computed(function() {
 
-  clientId: Cookies.get('keycloak-clientId'),
+    let cookieService = this.get('cookies');
+
+    let cookies = cookieService.read();
+
+    return keys(cookies).reduce((acc, key) => {
+      let value = cookies[key];
+      acc.push({ name: key, value });
+
+      return acc;
+    }, []);
+  }),
+
+  init() {
+
+    this._super(...arguments);
+
+    let cookies = this.get('cookies');
+
+    this.set('url', cookies.read('keycloak-url'));
+    this.set('realm', cookies.read('keycloak-realm'));
+    this.set('clientId', cookies.read('keycloak-clientId'));
+  },
 
   actions: {
 
     initKeycloak() {
 
-      let session = this.get('session');
+      let session = this.get('keycloakSession');
+      let cookies = this.get('cookies');
 
       let url = this.get('url');
       let realm = this.get('realm');
       let clientId = this.get('clientId');
 
       // save details as cookies for subsequent initializations
-      Cookies.set('keycloak-url', url);
-      Cookies.set('keycloak-realm', realm);
-      Cookies.set('keycloak-clientId', clientId);
+      cookies.write('keycloak-url', url);
+      cookies.write('keycloak-realm', realm);
+      cookies.write('keycloak-clientId', clientId);
 
       if (url && realm && clientId) {
 
--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -4,11 +4,12 @@
 import Route from '@ember/routing/route';
 import { get } from '@ember/object';
 import { inject as service } from '@ember/service';
-import Cookies from 'ember-cli-js-cookie';
 
 export default Route.extend({
 
   session: service('keycloak-session'),
+
+  cookies: service(),
 
   init() {
 
@@ -16,9 +17,10 @@ export default Route.extend({
 
     // if required constuctor parameters are available as cookies go ahead in init the service.
     // this would be replaced by initialization code when used in an application
-    let url = Cookies.get('keycloak-url');
-    let realm = Cookies.get('keycloak-realm');
-    let clientId = Cookies.get('keycloak-clientId');
+    let cookies = this.get('cookies');
+    let url = cookies.read('keycloak-url');
+    let realm = cookies.read('keycloak-realm');
+    let clientId = cookies.read('keycloak-clientId');
 
     if (url && realm && clientId) {
 
