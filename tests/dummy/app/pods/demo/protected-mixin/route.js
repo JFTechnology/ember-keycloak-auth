a4b27f25d72fb562f1104949a2130fed750030ce
Stephen Flynn <stephen.flynn@jftechnology.com>
2020-03-07 21:09:37 +0000

    fix: remove deprecated mixins

--- tests/dummy/app/pods/demo/protected-mixin/route.js
+++
@@ -1,6 +0,0 @@
-import Route from '@ember/routing/route';
-
-import KeycloakAuthenticatedRouteMixin from '@jftechnology/ember-keycloak-auth/mixins/keycloak-authenticated-route';
-
-export default class ProtectedMixinRoute extends Route.extend(KeycloakAuthenticatedRouteMixin) {
-}
